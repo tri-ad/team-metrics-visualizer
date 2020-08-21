6022932274d1827cf617b594e917cc0b851ab294
Moritz Scholz <moritz.scholz@tri-ad.global>
2020-08-21 14:58:32 +0900

    refactor: Overtime chart
    - Renamed some variables to make them more clear
    - Extracted method for the long lambda-function creating data labels

--- tmv/visuals/work_time.py
+++ tmv/visuals/work_time.py
@@ -1,11 +1,11 @@
 import logging
 from datetime import date, datetime, timedelta
 from typing import List, Optional, Union, Tuple
+from dateutil import parser as date_parser
 
 import dash_core_components as dcc
 import pandas as pd
 import plotly.graph_objects as go
-from dateutil import parser as du_parser
 from flask_security import current_user
 from sqlalchemy import and_, asc, desc
 
@@ -14,6 +14,8 @@ from structure.measurements import OTMeasurement
 from structure.organization import Team
 from visuals.base import VisualController
 from visuals.shared import fix_timedelta_plot
+
+DateLike = Union[date, datetime, str]
 
 
 class OvertimeChartController(VisualController):
@@ -24,8 +26,9 @@ class OvertimeChartController(VisualController):
 
     def __init__(self, chart_html_id: str = "overtime-chart"):
         self.chart_html_id = chart_html_id
-        self.title_format = "%B %Y"
+        self.date_format = "%B %Y"
         self.axis_label_period_format = "%B"
+        self.tick_format = "%H:%M"
 
     def get_latest_date(self) -> datetime:
         """ Return latest date for which data is available """
@@ -59,23 +62,20 @@ class OvertimeChartController(VisualController):
         """
         Draw bar-chart showing overtime data.
         """
-        # Get initial data and layout for chart
         data, layout = self.update(self.get_latest_date(), self.get_latest_date())
-
-        # Return the graph-object containing the barchart-figure.
         return dcc.Graph(
             id=self.chart_html_id, figure=go.Figure(data=data, layout=layout)
         )
 
-    def __safe_parse_to_ot_period(self, input_date: Union[date, datetime, str]) -> date:
+    def __safe_parse_to_ot_period(self, input_date: DateLike) -> date:
         """ Parse `input_date` to an overtime period of type `date` in a safe
             way """
 
         # Parse date with dateutil in case we were given a string.
         if isinstance(input_date, str):
-            input_date = du_parser.parse(input_date)
+            input_date = date_parser.parse(input_date)
 
-        # Cast input_date to `date`-type and set day=1
+        # (down)cast input_date to `date`-type and set day=1
         try:
             selected_period = date(year=input_date.year, month=input_date.month, day=1)
         except AttributeError:
@@ -97,7 +97,7 @@ class OvertimeChartController(VisualController):
         selected_end_date: date,
         selected_teams: Optional[List[int]] = None,
     ) -> pd.DataFrame:
-        # Set up SQL-filter for teams. If None, all teams are selected.
+        # Set up SQL-filter for teams. If selection is None, all teams are selected.
         if selected_teams is None:
             filter_teams = True
         else:
@@ -132,43 +132,48 @@ class OvertimeChartController(VisualController):
         """
         If data was successfully retrieved and is not empty, do the following:
             1. For each row, calculate overtime per day
-            2. Pivot the dataframe, so that it can be sliced by period and team
+            2. Group the entries, so that the data can be sliced by period and team
                 - Aggregate overtime per day by min, mean and max
                 - Calculate number of team members for the given period
-        If there was no data, return an empty dataframe and exit the function.
+            3. Sort the data by teams and dates
+        If there was no data, return an empty dataframe and return.
         """
         if result.empty:
-            logging.info("Result for the active filter is empty.")
+            logging.info("No overtime data for the active filter.")
             return pd.DataFrame()
 
-        # Calculate overtime per day.
+        # 1. Calculate overtime per day.
         # TODO: Make this safe for DivisionByZero-error.
         result["ot_per_day"] = result["overtime"] / result["workdays_actual"]
         # TODO: Need to fix
         result["num_members"] = 1
 
+        # 2. Group & aggregate
         data = result.groupby(["measurement_date", "team_name"]).agg(
             {"ot_per_day": list}
         )
-
         data.reset_index(inplace=True)
+
+        # 3. Sort by teams and dates & label each record
         data.sort_values(by=["team_name", "measurement_date"], inplace=True)
         data["label"] = data.apply(
-            lambda x: "%s %s"
-            % (
-                x["team_name"],
-                self.__safe_parse_to_ot_period(x["measurement_date"]).strftime(
-                    self.axis_label_period_format
-                ),
-            ),
-            axis=1,
+            lambda x: self._data_label(x["team_name"], x["measurement_date"]), axis=1,
         )
+
         return data
+
+    def _data_label(self, team_name: str, measurement_date: DateLike) -> str:
+        return "{team_name} {measurement_date}".format(
+            team_name=team_name,
+            measurement_date=self.__safe_parse_to_ot_period(measurement_date).strftime(
+                self.axis_label_period_format
+            ),
+        )
 
     def update(
         self,
-        selected_start_date: Union[date, datetime, str],
-        selected_end_date: Union[date, datetime, str],
+        selected_start_date: DateLike,
+        selected_end_date: DateLike,
         selected_teams: Optional[List[int]] = None,
     ) -> Tuple[List, go.Layout]:
         """
@@ -194,23 +199,25 @@ class OvertimeChartController(VisualController):
         traces = [
             go.Box(
                 x=[
-                    fix_timedelta_plot(i if i > timedelta(0) else timedelta(0))
-                    for i in row["ot_per_day"]
+                    fix_timedelta_plot(
+                        ot_per_day if ot_per_day > timedelta(0) else timedelta(0)
+                    )
+                    for ot_per_day in row["ot_per_day"]
                 ],
                 name=row["label"],
                 marker_color="#f56565",
             )
-            for i, row in data.iterrows()
+            for _, row in data.iterrows()
         ]
 
-        # Set title & layout of barchart
+        # Create readable title for the chart
         selected_start_period_readable = selected_start_period.strftime(
-            self.title_format
+            self.date_format
         )
-        selected_end_period_readable = selected_end_period.strftime(self.title_format)
+        selected_end_period_readable = selected_end_period.strftime(self.date_format)
         if selected_start_period_readable != selected_end_period_readable:
             title = (
-                f"Overtime for {selected_start_period_readable} ~"
+                f"Overtime for {selected_start_period_readable} to"
                 f" {selected_end_period_readable}"
             )
         else:
@@ -222,12 +229,11 @@ class OvertimeChartController(VisualController):
             legend_orientation="h",
             font=dict(size=12),
             bargap=0.2,
-            # Title appearing above the chart
             title=go.layout.Title(text=title),
             # Configure as a stacked bar chart
             barmode="stack",
             # X axis ticks
-            xaxis=dict(dtick=30 * 60 * 1000, tickformat="%H:%M"),
+            xaxis=dict(dtick=30 * 60 * 1000, tickformat=self.tick_format),
             # We reverse the yaxis, because it starts at the bottom by default.
             yaxis=dict(dtick=1, type="category", autorange="reversed", automargin=True),
             showlegend=False,
